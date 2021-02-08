77cb52ff9b218cd576f866054a9c0a7c97df61b5
ssnowicki <61975477+ssnowicki@users.noreply.github.com>
2021-02-08 16:19:24 -0600

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div id="login">
+      <a-tabs default-active-key="2">
+        <a-tab-pane key="1" tab="Password Login">
+          hi
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="Azure Login" force-render>
+          hi again
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Login",
+ 
+  data() {
+    return {
+    
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ tests/Login.spec.js
@@ -0,0 +1,34 @@
+/* global describe, it, expect */
+import { mount } from "@vue/test-utils";
+import Login from "../src/components/Login";
+import Antd from "ant-design-vue";
+
+describe("Login Success", () => {
+  it("Blank creds", async () => {
+    const wrapper = mount(Login, {
+      global: {
+        plugins: [Antd],
+        //stubs: ["a-tabs"],
+        mocks: {
+          $cookies: {
+            isKey: () => false,
+            get: (msg) => msg,
+            set: (msg) => msg
+          },
+          $route: {
+            params: {}
+          }
+        }
+      },
+      data() {
+        return {
+          input: {
+            username: "",
+            password: ""
+          }
+        };
+      }
+    });
+    expect(wrapper).toBeTruthy();
+  });
+});
